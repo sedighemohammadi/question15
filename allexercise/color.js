ff5bd5eb82c864636a3733f6d1eaf36169b919ae
sedighemohammadi <sedighemohammadi387@gmail.com>
2024-12-21 14:54:06 +0330

    3

---
+++ allexercise/color.js
@@ -0,0 +1,31 @@
+/*
+? You have to write a code that changes the color of rectangles upon clicking each color.
+
+- First, look at color-palette1.png . You should create a similar page.
+- There are seven colored circles, and upon clicking each circle, the rectangles will change their color. You can find the color codes for the circles below.
+- The color chosen will be applied to the first rectangle, and subsequent rectangles will be assigned colors from its spectrum.
+- hint: you can modify the alpha (a) of the rgba color to create the spectrum color 
+- For example, you can refer to photo color-palette2.png 2, it is for the time when the green color is clicked.
+
+rgba(31, 127, 102, 1)
+rgba(255, 200, 0, 1)
+rgba(255, 123, 0, 1)
+rgba(216, 1, 1, 1)
+rgba(223, 4, 70, 1)
+rgba(127, 31, 85, 1) 
+rgba(93, 18, 199, 1)
+rgba(93, 18, 199, 1)
+*/
+
+function changeColor(r, g, b) {
+    const rect1 = document.getElementById('rect1');
+    const rect2 = document.getElementById('rect2');
+    const rect3 = document.getElementById('rect3');
+
+    
+    rect1.style.backgroundColor = `rgba(${r}, ${g}, ${b}, 1)`;
+
+    
+    rect2.style.backgroundColor = `rgba(${r}, ${g}, ${b}, 0.7)`;
+    rect3.style.backgroundColor = `rgba(${r}, ${g}, ${b}, 0.4)`;
+}
